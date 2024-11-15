c466912eae1e5ad6be58b71f273dd2dded94b25d
YANOasahi <asahiyjp@gmail.com>
2024-11-15 18:33:15 +0900

    fine

--- Enge.py
+++ Enge.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import variables_Enge
 
 # define 6order Enge function
-def Enge(x):
+def enge(x):
     return 1/(1+(np.exp(variables_Enge.enge[0]+(x*variables_Enge.enge[1])+\
                 (x**2*variables_Enge.enge[2])+(x**3*variables_Enge.enge[3])+\
                 (x**4*variables_Enge.enge[4])+(x**5*variables_Enge.enge[5]))))
@@ -14,7 +14,7 @@ def Enge(x):
 
 
 # check the shape of the function
-x = np.arange(-10, 10, 0.5)
+# x = np.arange(-10, 10, 0.5)
 
-plt.plot(x, Enge(x))
-plt.show()
+# plt.plot(x, Enge(x))
+# plt.show()
