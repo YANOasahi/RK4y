c15e21768d02df6aef5504345c9f7c7bec7276cf
YANOasahi <asahiyjp@gmail.com>
2024-12-03 18:30:53 +0900

    ok

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import variables_conditions as vc
 
 #  set brho
@@ -7,4 +8,22 @@ brho = vc.brho0*(1+dp/100)
 
 # set dispersion [m/%] in the ring
 dispersion = -3.3494*dp**6 + 2.2088*dp**5 + 1.5699*dp**4 - \
-       0.731*dp**3 - 0.1573*dp**2 + 0.0121*dp + 7.0737
+            0.731*dp**3 - 0.1573*dp**2 + 0.0121*dp + 7.0737
+
+# m/q
+MoQ=vc.mass/vc.z
+# momentum based on brho
+mom=vc.c*vc.z*brho*1E-6
+# beam energy based on momentum
+energy = (np.sqrt(mom**2+(vc.mass*vc.amu)**2)-(vc.mass*vc.amu))/vc.mass
+# beta based on brho and q/m
+beta = ((vc.c*vc.z*brho)/(vc.amu*vc.mass))/np.sqrt(((vc.c*vc.z*brho)/(vc.amu*vc.mass))*((vc.c*vc.z*brho)/(vc.amu*vc.mass))+1)
+# Lorentz factor
+gamma=1/np.sqrt(1-beta**2)
+# velocity of the beam [mm/ns]
+v=vc.c*beta*1E-6
+
+print(f'Proton number of the beam is {vc.z}')
+print(f'Mass of the beam is {vc.mass}')
+print(f'Mass to charge ratio of the beam is {MoQ}')
+print(f'Beam energy is {energy*1E6} MeV/u')
--- variables_conditions.py
+++ variables_conditions.py
@@ -15,12 +15,12 @@ brho0 = 4.7447
 betax = 7.817
 
 
-# _______basicaly we don't have to change below_______
+# _______basically we don't have to change below_______
 # current of the trim coils
 trim_current = [592.128, 1.958, 91.045, 226.784,
                 158.111, 146.1, 117.213, 280.012, 47.809, 279.008]
 
-# caluclation is normalized by this current
+# calculation is normalized by this current
 main_base = 1915
 
 # set magnetic flux density
