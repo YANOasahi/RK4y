b64001ef9590945e17e4a4dc53b068722791c00a
YANOasahi <asahiyjp@gmail.com>
2024-12-18 19:21:02 +0900

    for debugging

--- compare2.py
+++ compare2.py
@@ -10,9 +10,19 @@ fig1_1 = box1.add_subplot(1, 1, 1)
 # fig1_1 = plt.plot(data1[:,0], data1[:,1], label="Yano 1e-6")
 
 data2=np.genfromtxt('./bzz_yano2.dat')
-fig1_1 = plt.plot(data2[:,0], data2[:,1], label="Yano 1e-10")
+fig1_1 = plt.plot(data2[:,0]*1000, data2[:,1], label="Yano 1e-10")
 
-data3=np.genfromtxt('./bzz_abesan.dat')
+# remove dupulication
+input_file = "bzz_abesan.dat"  # origin
+output_file = "bzz_abesan2.dat"  # new output
+
+with open(input_file, "r") as infile:
+    unique_lines = sorted(set(line.strip() for line in infile))
+
+with open(output_file, "w") as outfile:
+    outfile.write("\n".join(unique_lines) + "\n")
+
+data3=np.genfromtxt('./bzz_abesan2.dat')
 fig1_1 = plt.plot(data3[:,0], data3[:,1], label="Abe-san 1e-10")
 
 plt.legend()
