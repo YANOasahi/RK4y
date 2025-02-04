925f2653d93f4ac752574b2707ce9b063b833ec0
YANOasahi <asahiyjp@gmail.com>
2025-02-04 14:25:41 +0900

    old map system

---
+++ dump/mag_field.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+import map
+import variables as vs
+import position as pos
+
+notrim_map, trim_map = map.map_making()
+
+def mag_field(r):
+    # find the nearest magnet
+    x, y, z = r
+    distance_mag=[[((vs.magnet_pos_x[i][j]-(x*1000))**2 + 
+                    (vs.magnet_pos_y[i][j]-(y*1000))**2 +
+                    (vs.magnet_pos_z[i][j]-z*1000)**2) for j in range(4)] for i in range(6)]
+    near_mag = np.unravel_index(np.argmin(distance_mag), (6, 4))
+    min_distance_mag=distance_mag[near_mag[0]][near_mag[1]]
+
+    # if the nearest magnet is too far,
+    # magnetic field will be np.array([0, 0, 0])
+    if min_distance_mag > 1000**2 + 200**2 + 18**2:
+        print('too far!')
+        return np.array([0, 0, 0])
+    
+    elif near_mag[1]==0 or near_mag[1]==3:
+        # convert particle position to the nearest magnet's coordinate
+        particle_pos = pos.Position(x*1000, y*1000, z,
+                               vs.magnet_pos_x[near_mag[0]][near_mag[1]],
+                               vs.magnet_pos_y[near_mag[0]][near_mag[1]],
+                               vs.bend_angle[near_mag[0]][near_mag[1]])
+        # find the nearest map data
+        pos_in_map = [((trim_map[i][0]-particle_pos[0][1])**2 + 
+                       (trim_map[i][1]-particle_pos[0][0])**2 +
+                       (trim_map[i][2]-particle_pos[0][2])**2) 
+                       for i in range(16800000)]
+        nearest = np.argmin(pos_in_map)
+        print('trim magnet')
+        return np.array(
+            [notrim_map[nearest][4], 
+             notrim_map[nearest][3], 
+             notrim_map[nearest][5]]
+            )
+    
+    elif near_mag[1]==1 or near_mag[1]==2:
+        # convert particle position to the nearest magnet's coordinate
+        particle_pos = pos.Position(x*1000, y*1000, z,
+                               vs.magnet_pos_x[near_mag[0]][near_mag[1]],
+                               vs.magnet_pos_y[near_mag[0]][near_mag[1]],
+                               vs.bend_angle[near_mag[0]][near_mag[1]])
+        # find the nearest map data
+        pos_in_map = [((notrim_map[i][0]-particle_pos[0][1])**2 + 
+                       (notrim_map[i][1]-particle_pos[0][0])**2 +
+                       (notrim_map[i][2]-particle_pos[0][2])**2) 
+                       for i in range(16800000)]
+        nearest = np.argmin(pos_in_map)
+        print('no trim magnet')
+        return np.array(
+            [trim_map[nearest][4], 
+             trim_map[nearest][3], 
+             trim_map[nearest][5]]
+            )
---
+++ dump/notrim_map.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import glob
+
+# read map files
+file_list = glob.glob("./notrim_map/*.txt")
+
+# prepare pandas for the data from the files
+map_tmp = [pd.read_table(file, header=None) for file in file_list]
+
+# conect pandas
+buffer = pd.concat(map_tmp, ignore_index=True)
+
+# convert pandas to ndarray
+map = buffer.to_numpy()
+
+# # check
+# print(result_array.shape)
+
+# the position of particle
+particle = np.array(
+    [np.random.randint(-1000, 1000),
+     np.random.randint(-200, 200),
+     np.random.randint(-20, 20)]
+)
+
+print(f'particle position is {particle}')
+
+# pick the nearest point from the map data
+distance = [((map[i][0]-particle[0])**2 + (map[i][1]-particle[1]) ** 2 +
+            (map[i][2]-abs(particle[2]))**2) for i in range(16800000)]
+min = np.argmin(distance)
+
+# check
+print(f'min is {min}')
+print(f'distance[{min}] is {distance[min]}')
+
+if particle[2] >= 0:
+    print(f'the nearest point is ({map[min][0]},{map[min][1]},{map[min][2]})')
+    print(f'B is ({map[min][3]},{map[min][4]},{map[min][5]})')
+else:
+    print(f'the nearest point is ({map[min][0]},{map[min][1]},{-map[min][2]})')
+    print(f'B is ({map[min][3]},{-map[min][4]},{map[min][5]})')
