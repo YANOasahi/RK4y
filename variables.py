a2d16ccab6733337b79388375a9973ed3680d001
YANOasahi <r3-lenovoe14@R3-LenovoE14.>
2025-02-04 21:02:12 +0900

    ok

--- variables.py
+++ variables.py
@@ -4,7 +4,7 @@ import numpy as np
 # current of the main magnets
 # main_current = 1918.1205
 # main_current = 1918.4094  # this value is the best fit with abe-san's code
-main_current = 1910.0 # return to almost the same position
+main_current = 1910.1 # return to almost the same position
 
 # calculation is normalized by this current
 main_base = 1915.0
